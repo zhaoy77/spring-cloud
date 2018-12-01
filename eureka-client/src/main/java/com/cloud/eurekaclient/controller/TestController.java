7d121f90d83160dc5b3fffed4a564167c1fa3d4b
wayne <wayne@laitouzhu.com>
2018-12-01 17:45:14 +0800

    init

---
+++ eureka-client/src/main/java/com/cloud/eurekaclient/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.cloud.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wayne
+ */
+@RestController
+public class TestController {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
---
+++ eureka-consumer/src/main/java/com/cloud/eurekaconsumer/controller/DcController.java
@@ -0,0 +1,29 @@
+package com.cloud.eurekaconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Wayne
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("EUREKA-CLIENT");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
